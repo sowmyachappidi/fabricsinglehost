b7d75d8b69bd23fa051546d9a078bd893105de1b
Ubuntu <ubuntu@vm1.asia-south1-c.c.regal-mediator-342706.internal>
2022-06-05 14:33:45 +0000

    second

--- setup1/vm1/create-certificate-with-ca/create-certificate-with-ca.sh
+++ setup1/vm1/create-certificate-with-ca/create-certificate-with-ca.sh
@@ -27,10 +27,6 @@ createcertificatesForOrg1() {
   echo
   fabric-ca-client register --caname ca.org1.example.com --id.name peer0 --id.secret peer0pw --id.type peer --tls.certfiles ${PWD}/fabric-ca/org1/tls-cert.pem
 
-  echo
-  echo "Register peer1"
-  echo
-  fabric-ca-client register --caname ca.org1.example.com --id.name peer1 --id.secret peer1pw --id.type peer --tls.certfiles ${PWD}/fabric-ca/org1/tls-cert.pem
 
   echo
   echo "Register user"
@@ -75,25 +71,6 @@ createcertificatesForOrg1() {
 
   # ------------------------------------------------------------------------------------------------
 
-  # Peer1
-
-  mkdir -p ../crypto-config/peerOrganizations/org1.example.com/peers/peer1.org1.example.com
-
-  echo
-  echo "## Generate the peer1 msp"
-  echo
-  fabric-ca-client enroll -u https://peer1:peer1pw@localhost:7054 --caname ca.org1.example.com -M ${PWD}/../crypto-config/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/msp --csr.hosts peer1.org1.example.com --tls.certfiles ${PWD}/fabric-ca/org1/tls-cert.pem
-
-  cp ${PWD}/../crypto-config/peerOrganizations/org1.example.com/msp/config.yaml ${PWD}/../crypto-config/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/msp/config.yaml
-
-  echo
-  echo "## Generate the peer1-tls certificates"
-  echo
-  fabric-ca-client enroll -u https://peer1:peer1pw@localhost:7054 --caname ca.org1.example.com -M ${PWD}/../crypto-config/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls --enrollment.profile tls --csr.hosts peer1.org1.example.com --csr.hosts localhost --tls.certfiles ${PWD}/fabric-ca/org1/tls-cert.pem
-
-  cp ${PWD}/../crypto-config/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/tlscacerts/* ${PWD}/../crypto-config/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/ca.crt
-  cp ${PWD}/../crypto-config/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/signcerts/* ${PWD}/../crypto-config/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/server.crt
-  cp ${PWD}/../crypto-config/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/keystore/* ${PWD}/../crypto-config/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/server.key
 
   # --------------------------------------------------------------------------------------------------
 
