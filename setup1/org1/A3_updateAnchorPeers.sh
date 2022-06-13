6b41270df78ed4826451507922e99fb037ca5a9b
MEHER CHAITANYA <47631184+meherchaitanyabandaru@users.noreply.github.com>
2022-06-13 16:26:25 +0530

    Update A3_updateAnchorPeers.sh

--- setup1/org1/A3_updateAnchorPeers.sh
+++ setup1/org1/A3_updateAnchorPeers.sh
@@ -12,12 +12,12 @@ setGlobalsForPeer0Org1(){
     export CORE_PEER_ADDRESS=localhost:7051
 }
 
-setGlobalsForPeer1Org1(){
-    export CORE_PEER_LOCALMSPID="Org1MSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
-    export CORE_PEER_MSPCONFIGPATH=${PWD}/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
-    export CORE_PEER_ADDRESS=localhost:8051
-}
+#setGlobalsForPeer1Org1(){
+ #   export CORE_PEER_LOCALMSPID="Org1MSP"
+  #  export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
+   # export CORE_PEER_MSPCONFIGPATH=${PWD}/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+   # export CORE_PEER_ADDRESS=localhost:8051
+#}
 
 
 updateAnchorPeers(){
