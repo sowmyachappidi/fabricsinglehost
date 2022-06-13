588246aa7a1f31bcc60f9b47582337fafcedf515
mehar <mehar@mehars-MacBook-Air.local>
2022-06-13 23:45:21 +0530

    New org automation added

---
+++ setup1/addOrg4/scripts/3_createConfigUpdate.sh
@@ -0,0 +1,72 @@
+export CORE_PEER_TLS_ENABLED=true
+export ORDERER_CA=${PWD}/../../artifacts/channel/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+export PEER0_ORG1_CA=${PWD}/../../artifacts/channel/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export PEER0_ORG2_CA=${PWD}/../../artifacts/channel/crypto-config/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+export PEER0_ORG4_CA=${PWD}/../crypto-config/peerOrganizations/org4.example.com/peers/peer0.org4.example.com/tls/ca.crt
+export FABRIC_CFG_PATH=${PWD}/../../artifacts/channel/config/
+
+export CHANNEL_NAME=mychannel
+
+setGlobalsForOrderer() {
+    export CORE_PEER_LOCALMSPID="OrdererMSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/../../artifacts/channel/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/../../artifacts/channel/crypto-config/ordererOrganizations/example.com/users/Admin@example.com/msp
+
+}
+
+setGlobalsForPeer0Org1() {
+    export CORE_PEER_LOCALMSPID="Org1MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/../../artifacts/channel/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+    export CORE_PEER_ADDRESS=localhost:7051
+}
+
+setGlobalsForPeer0Org2() {
+    export CORE_PEER_LOCALMSPID="Org2MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG2_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/../../artifacts/channel/crypto-config/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+    export CORE_PEER_ADDRESS=localhost:9051
+
+}
+
+setGlobalsForPeer0Org4() {
+    export CORE_PEER_LOCALMSPID="Org4MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG4_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/../crypto-config/peerOrganizations/org4.example.com/users/Admin@org4.example.com/msp
+    export CORE_PEER_ADDRESS=localhost:11051
+
+}
+
+
+createConfigUpdate() {
+
+    CHANNEL="mychannel"
+    ORIGINAL="config.json"
+    MODIFIED="modified_config.json"
+    OUTPUT="org4_update_in_envelope.pb"
+
+    set -x
+    
+    echo "Step 3.1 Encode Original Config"
+    configtxlator proto_encode --input "${ORIGINAL}" --type common.Config >original_config.pb
+    
+    echo "Step 3.2 Encode Modified Config"
+    configtxlator proto_encode --input "${MODIFIED}" --type common.Config >modified_config.pb
+    
+    echo "Step 3.3  Compute Update"
+    configtxlator compute_update --channel_id "${CHANNEL}" --original original_config.pb --updated modified_config.pb >config_update.pb
+    
+    echo "Step 3.4 Config Update"
+    configtxlator proto_decode --input config_update.pb --type common.ConfigUpdate >config_update.json
+    
+    echo "Step 3.5 Config update Envolope"
+    echo '{"payload":{"header":{"channel_header":{"channel_id":"'$CHANNEL'", "type":2}},"data":{"config_update":'$(cat config_update.json)'}}}' | jq . >config_update_in_envelope.json
+    
+    echo "Step 3.6 Envolope"
+    configtxlator proto_encode --input config_update_in_envelope.json --type common.Envelope >"${OUTPUT}"
+    
+    set +x
+
+}
+
+createConfigUpdate
---
+++ setup1/addOrg4/scripts/4_signConfigAsPeerOrg.sh
@@ -0,0 +1,55 @@
+export CORE_PEER_TLS_ENABLED=true
+export ORDERER_CA=${PWD}/../../artifacts/channel/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+export PEER0_ORG1_CA=${PWD}/../../artifacts/channel/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export PEER0_ORG2_CA=${PWD}/../../artifacts/channel/crypto-config/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+export PEER0_ORG4_CA=${PWD}/../crypto-config/peerOrganizations/org4.example.com/peers/peer0.org4.example.com/tls/ca.crt
+export FABRIC_CFG_PATH=${PWD}/../../artifacts/channel/config/
+
+export CHANNEL_NAME=mychannel
+
+setGlobalsForOrderer() {
+    export CORE_PEER_LOCALMSPID="OrdererMSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/../../artifacts/channel/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/../../artifacts/channel/crypto-config/ordererOrganizations/example.com/users/Admin@example.com/msp
+
+}
+
+setGlobalsForPeer0Org1() {
+    export CORE_PEER_LOCALMSPID="Org1MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/../../artifacts/channel/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+    export CORE_PEER_ADDRESS=localhost:7051
+}
+
+setGlobalsForPeer0Org2() {
+    export CORE_PEER_LOCALMSPID="Org2MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG2_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/../../artifacts/channel/crypto-config/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+    export CORE_PEER_ADDRESS=localhost:9051
+
+}
+
+setGlobalsForPeer0Org4() {
+    export CORE_PEER_LOCALMSPID="Org4MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG4_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/../crypto-config/peerOrganizations/org4.example.com/users/Admin@org4.example.com/msp
+    export CORE_PEER_ADDRESS=localhost:11051
+
+}
+
+
+signConfigAsPeerOrg() {
+
+    echo "Step 4.1 Sign from ORG -1"
+    setGlobalsForPeer0Org1
+    peer channel signconfigtx -f org4_update_in_envelope.pb
+    
+    echo "Step 4.2 Update & Sign from ORG -2"
+    setGlobalsForPeer0Org2
+    peer channel update -f org4_update_in_envelope.pb -c ${CHANNEL_NAME} \
+        -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com \
+        --tls --cafile ${ORDERER_CA}
+
+}
+
+signConfigAsPeerOrg
