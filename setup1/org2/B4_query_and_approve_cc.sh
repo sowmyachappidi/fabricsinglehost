7f30465809fbc7e2028b8df22d81472d313b43c9
MEHER CHAITANYA <47631184+meherchaitanyabandaru@users.noreply.github.com>
2022-06-13 16:34:52 +0530

    Update B4_query_and_approve_cc.sh

--- setup1/org2/B4_query_and_approve_cc.sh
+++ setup1/org2/B4_query_and_approve_cc.sh
@@ -13,13 +13,13 @@ setGlobalsForPeer0Org2() {
 
 }
 
-setGlobalsForPeer1Org2() {
-    export CORE_PEER_LOCALMSPID="Org2MSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG2_CA
-    export CORE_PEER_MSPCONFIGPATH=${PWD}/crypto-config/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
-    export CORE_PEER_ADDRESS=localhost:10051
+#setGlobalsForPeer1Org2() {
+ #   export CORE_PEER_LOCALMSPID="Org2MSP"
+ #   export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG2_CA
+ #   export CORE_PEER_MSPCONFIGPATH=${PWD}/crypto-config/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+ #   export CORE_PEER_ADDRESS=localhost:10051
 
-}
+#}
 
 CHANNEL_NAME="mychannel"
 CC_RUNTIME_LANGUAGE="golang"
